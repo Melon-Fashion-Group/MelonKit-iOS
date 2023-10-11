821ee936b0e4e2fb546d52504386bcbe9fb0f64d
Dmitry Novikov <45120847+shakendev@users.noreply.github.com>
2023-10-11 11:04:17 +0300

    feat(hardware): adding haptic feedback service

---
+++ Sources/MelonKit/Hardware/Haptic Feedbacks/Haptic Feedback/Extensions/Enumerations/MLNHaptic.FeedbackIntensity.swift
@@ -0,0 +1,50 @@
+//
+//  MLNHaptic.FeedbackIntensity.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 06.10.2023.
+//  Copyright © 2023 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import Foundation
+
+
+
+// MARK: - MLNHaptic.FeedbackIntensity
+
+extension MLNHaptic {
+
+    ///
+    /// An enumeration that describes a list of feedback intensities.
+    ///
+    public enum FeedbackIntensity: CGFloat {
+
+        ///
+        ///
+        ///
+        case weak = 0.2
+
+        ///
+        ///
+        ///
+        case low = 0.4
+
+        ///
+        ///
+        ///
+        case medium = 0.6
+
+        ///
+        ///
+        ///
+        case high = 0.8
+
+        ///
+        ///
+        ///
+        case strong = 1
+    }
+}
