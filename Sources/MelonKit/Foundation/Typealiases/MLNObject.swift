f07087d6f2d06c81435cab233d47c6015e7f66a4
Dmitry Novikov <45120847+shakendev@users.noreply.github.com>
2023-10-11 11:01:00 +0300

    feat(foundation): Adding fundamental types of the framework

---
+++ Sources/MelonKit/Foundation/Typealiases/MLNObject.swift
@@ -0,0 +1,22 @@
+//
+//  MLNObject.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 06.10.2023.
+//  Copyright © 2023 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import Foundation
+
+
+
+// MARK: - MLNObject
+
+///
+/// An alias for AnyObject & CustomStringConvertible types used to extend MelonKit services.
+///
+@available(iOS 15.0, *)
+public typealias MLNObject = AnyObject & CustomStringConvertible
