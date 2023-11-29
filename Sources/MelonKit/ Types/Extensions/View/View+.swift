3e5b317dd7de764c4c30218efbb37f880f8f4fcb
Dmitry Novikov <45120847+shakendev@users.noreply.github.com>
2023-11-29 15:31:10 +0300

    feat(view): dimming detection & execute new task features

---
+++ Sources/MelonKit/ Types/Extensions/View/View+.swift
@@ -0,0 +1,74 @@
+//
+//  View+.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 29.11.2023.
+//  Copyright © 2023 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import SwiftUI
+
+
+
+// MARK: - Execute New Task
+
+extension View {
+
+    ///
+    ///
+    ///
+    @available(iOS 16.0, *)
+    public func executeNewTask(
+        delay: Float = 0,
+        body: (() -> Void)? = nil,
+        completion: () -> Void
+    ) async {
+        try? await Task.sleep(nanoseconds: .init(delay * 1_000_000_000))
+
+        body?()
+
+        await MainActor.run {
+            completion()
+        }
+    }
+}
+
+
+
+// MARK: -  Render the View into an Image
+
+extension View {
+
+    ///
+    ///
+    ///
+    @available(iOS 16.0, *)
+    @MainActor
+    public func renderImage(from view: some View) -> UIImage? {
+        let renderer = ImageRenderer(content: view)
+
+        return renderer.uiImage
+    }
+}
+
+
+
+// MARK: - View Darkness Detection
+
+extension View {
+
+    ///
+    ///
+    ///
+    @available(iOS 16.0, *)
+    public func isDark(_ view: some View) async -> Bool {
+        guard let image = await renderImage(from: view) else {
+            return false
+        }
+
+        return await image.isDark()
+    }
+}
