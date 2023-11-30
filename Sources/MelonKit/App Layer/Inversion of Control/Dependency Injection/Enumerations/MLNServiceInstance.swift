ed379f698ebc7afe9d540d0e519c2e0fc237624a
Dmitry Novikov <45120847+shakendev@users.noreply.github.com>
2023-12-01 01:45:49 +0300

    feat(service): adding DI and DI Container services

---
+++ Sources/MelonKit/App Layer/Inversion of Control/Dependency Injection/Enumerations/MLNServiceInstance.swift
@@ -0,0 +1,33 @@
+//
+//  MLNServiceInstance.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 01.12.2023.
+//  Copyright © 2023 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import Foundation
+
+
+
+// MARK: - MLNServiceInstance
+
+///
+///
+///
+@available(iOS 16.0, *)
+public enum MLNServiceInstance {
+
+    ///
+    ///
+    ///
+    case new
+
+    ///
+    ///
+    ///
+    case same
+}
---
+++ Sources/MelonKit/App Layer/Inversion of Control/Dependency Injection/Enumerations/MLNServiceType.swift
@@ -0,0 +1,33 @@
+//
+//  MLNServiceType.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 01.12.2023.
+//  Copyright © 2023 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import Foundation
+
+
+
+// MARK: - MLNServiceType
+
+///
+///
+///
+@available(iOS 16.0, *)
+public enum MLNServiceType {
+
+    ///
+    ///
+    ///
+    case new
+
+    ///
+    ///
+    ///
+    case singleton(instance: MLNServiceInstance)
+}
